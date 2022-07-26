5db7444e10c82b4f03967daf06baca6a0300e5ec
bartvanbenthem <bartvanbenthem@hotmail.com>
2022-07-26 18:50:08 +0200

    update posts

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -6,13 +6,15 @@ import (
 	"time"
 
 	"github.com/bartvanbenthem/gofound-blogger/internal/data"
+	"github.com/bartvanbenthem/gofound-blogger/internal/validator"
 )
 
 func (app *application) createPostsHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string `json:"title"`
-		Content string `json:"content"`
-		Author  string `json:"author,omitempty"`
+		Title   string   `json:"title"`
+		Content string   `json:"content"`
+		Author  string   `json:"author,omitempty"`
+		ImgURLs []string `json:"img_urls,omitempty"`
 	}
 
 	err := app.readJSON(w, r, &input)
@@ -20,6 +22,22 @@ func (app *application) createPostsHandler(w http.ResponseWriter, r *http.Reques
 		app.badRequestResponse(w, r, err)
 		return
 	}
+
+	post := &data.Post{Title: input.Title,
+		Content: input.Content,
+		Author:  input.Author,
+		ImgURLs: input.ImgURLs}
+
+	// Initialize a new Validator.
+	v := validator.New()
+
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidatePost(v, post); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
--- internal/data/posts.go
+++ internal/data/posts.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"github.com/bartvanbenthem/gofound-blogger/internal/validator"
+)
 
 type Post struct {
 	ID        int64     `json:"id"`
@@ -8,4 +12,13 @@ type Post struct {
 	Title     string    `json:"title"`
 	Content   string    `json:"content"`
 	Author    string    `json:"author,omitempty"`
+	ImgURLs   []string  `json:"img_urls,omitempty"`
+}
+
+func ValidatePost(v *validator.Validator, post *Post) {
+	v.Check(post.Title != "", "title", "must be provided")
+	v.Check(len(post.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(post.Content != "", "content", "must be provided")
+	v.Check(len(post.Title) <= 10000, "content", "must not be more than 10000 bytes long")
+	v.Check(validator.Unique(post.ImgURLs), "img_urls", "must not contain duplicate values")
 }
