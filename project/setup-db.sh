71484cc4104db81b2dd5eb7187be6038e1d07f98
bartvanbenthem <bartvanbenthem@hotmail.com>
2022-07-27 14:43:09 +0200

    update

--- project/setup-db.sh
+++ project/setup-db.sh
@@ -11,7 +11,8 @@ exit;
 export BLOG_DB_DSN='postgres://postgres:password@localhost/blog?sslmode=disable'
 
 # install migrate tool
-sudo cp golang-migrate/migrate.linux-amd64 $GOPATH/bin/migrate
+curl -L https://github.com/golang-migrate/migrate/releases/download/v4.14.1/migrate.linux-amd64.tar.gz | tar xvz
+sudo mv migrate.linux-amd64 $GOPATH/bin/migrate
 migrate --version
 
 # create new migration files
